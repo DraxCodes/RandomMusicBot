895fdd35d2039e6cf8d25e04eca2a6725b2207fe
Joel Parkinson <joelparkinson@live.co.uk>
2019-03-31 18:41:50 +0100

    Implement Storage

---
+++ MusicBot/Storage/IStorage.cs
@@ -0,0 +1,14 @@
+﻿using MusicBot.Storage.Entities;
+using System.Threading.Tasks;
+
+namespace MusicBot.Storage
+{
+    public interface IStorage
+    {
+        Task<T> Retreive<T>(string path);
+        Task Store(object obj, string path);
+        bool FileExists(string path);
+        void CreatePath(string path);
+        void InitializeResources(string directory);
+    }
+}
---
+++ MusicBot/Storage/PersistentStorage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using MusicBot.Storage.Entities;
+using Newtonsoft.Json;
+
+namespace MusicBot.Storage
+{
+    class PersistentStorage : IStorage
+    {
+        public void CreatePath(string path)
+        {
+            File.WriteAllText(path, "");
+        }
+
+        public void InitializeResources(string directory)
+        {
+            Directory.CreateDirectory(directory);
+        }
+
+        public bool FileExists(string path)
+        {
+            if (File.Exists(path))
+                return true;
+            else
+                return false;
+        }
+
+        public async Task<T> Retreive<T>(string path)
+        {
+            var json = await File.ReadAllTextAsync(path);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public async Task Store(object obj, string path)
+        {
+            var json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            await File.WriteAllTextAsync(path, json);
+        }
+    }
+}
