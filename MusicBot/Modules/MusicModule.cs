853c24e2756c1d65891321efbad8cdbc5eae88bb
Joel Parkinson <joelparkinson@live.co.uk>
2019-03-31 18:44:01 +0100

    Implement Music Functionality

---
+++ MusicBot/Modules/MusicModule.cs
@@ -0,0 +1,132 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using MusicBot.Discord;
+using MusicBot.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MusicBot.Modules
+{
+    public class MusicModule : ModuleBase<ShardedCommandContext>
+    {
+        private MusicService _musicService;
+        private DiscordShardedClient _client;
+
+        public MusicModule(MusicService musicService, DiscordShardedClient client)
+        {
+            _musicService = musicService;
+            _client = client;
+        }
+
+        [Command("Join")]
+        public async Task Join()
+        {
+            var user = Context.User as SocketGuildUser;
+            if (user.VoiceChannel != null)
+            {
+                await _musicService.ConnectAsync(user.VoiceChannel, Context.Channel as ITextChannel);
+                await ReplyAsync($"Now connected to: {user.VoiceChannel.Name}");
+            }
+            else
+            {
+                await ReplyAsync("Join a voice channel to use this command.");
+                var chan = _client.GetChannel(377879473644765185) as SocketTextChannel;
+                var msg = await chan.GetMessageAsync(559761812321468459);
+                var author = msg.Author;
+            }
+
+        }
+
+        [Command("Leave")]
+        public async Task Leave()
+            => await _musicService.LeaveAsync();
+
+        [Command("Play")]
+        public async Task Play([Remainder]string search)
+        {
+            var musicInfo = await _musicService.PlayAsync(search, Context.Guild.Id);
+            var embed = new EmbedBuilder()
+                .WithTitle(musicInfo.Title)
+                .WithThumbnailUrl(musicInfo.ImageUrl)
+                .WithDescription($"Author: {musicInfo.Author}\nLength: 0:00 - {musicInfo.Length:hh\\:mm\\:ss}") //TODO FIX THIS & Add check for hours.
+                .WithUrl(musicInfo.Url);
+
+            await ReplyAsync(embed: embed.Build());
+        }
+
+        [Command("Stop")]
+        public async Task Stop()
+            => await ReplyAsync(await _musicService.StopAsync());
+
+        [Command("Skip")]
+        public async Task Skip()
+            => await ReplyAsync(await _musicService.SkipAsync());
+
+        [Command("Pause"), Alias("Resume")]
+        public async Task Pause()
+            => await ReplyAsync(await _musicService.PauseOrResumeAsync());
+
+        [Command("Volume")]
+        public async Task test(int vol)
+            => await _musicService.SetVolumeAsync(vol);
+
+        [Command("List")]
+        public async Task AddRole()
+        {
+            var queue = _musicService.GetQueue();
+            if (queue.Count() is 0)
+            {
+                await ReplyAsync("Nothing in the queue");
+                return;
+            }
+
+            var sb = new StringBuilder();
+            foreach (var track in queue)
+            {
+                var title = string.Empty;
+                if (track.Title.Length > 40)
+                {
+                    title = track.Title.Remove(40, (track.Title.Length - 40));
+                    title += "...";
+                }
+                else
+                {
+                    title = track.Title;
+                }
+
+                sb.Append($"[{title}]({track.Uri})\n");
+            }
+
+            var embed = new EmbedBuilder()
+                .WithTitle($"Current Queue: {queue.Count()} Items.")
+                .WithDescription($"{sb}")
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp();
+            await ReplyAsync(embed: embed.Build());
+        }
+
+        [Command("Test")]
+        [RequireOwner]
+        public async Task Test()
+        {
+            //The input
+            var test = "kqtrwqkekqwjeyqwkyekljqwyelkqweliwqyelkwqehliqwyelkqwheqywelqwhelqweoiqwelqwleiuqweuqwe";
+
+            //Regex way
+            var regex = new Regex("[^e]");
+            var newString = regex.Replace(test, "");
+            Console.WriteLine(newString.Length);
+
+            //Linq way
+            var tetste = test.Where(x => x.Equals('e')).Count();
+            Console.WriteLine(tetste);
+        }
+    }
+}
---
+++ MusicBot/Services/MusicService.cs
@@ -0,0 +1,174 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Heaven_s_Bot.Core.UserAccounts;
+using MusicBot.Core.Entities;
+using MusicBot.Core.Logger;
+using MusicBot.Discord.EntityConverters;
+using MusicBot.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Victoria;
+using Victoria.Entities;
+
+namespace MusicBot.Services
+{
+    public class MusicService : IServiceExtension
+    {
+        private DiscordShardedClient _client;
+        private LavaShardClient _lavaSocketClient;
+        private LavaRestClient _lavaRestClient;
+        private LavaPlayer _player;
+        private ILogger _logger;
+
+        public MusicService(DiscordShardedClient client, LavaShardClient lavaSocketClient, LavaRestClient lavaRestClient, ILogger logger)
+        {
+            _client = client;
+            _lavaSocketClient = lavaSocketClient;
+            _lavaRestClient = lavaRestClient;
+            _logger = logger;
+        }
+
+        public Task InitializeAsync()
+        {
+            _client.ShardReady += ClientShardReady; ;
+            _lavaSocketClient.Log += LavaLogAsync;
+            _lavaSocketClient.OnTrackFinished += LavaTrackFinishedAsync;
+            _lavaSocketClient.OnServerStats += DisplayStatsAsync;
+            return Task.CompletedTask;
+        }
+
+        private async Task ClientShardReady(DiscordShardedClient client)
+        {
+            await _lavaSocketClient.StartAsync(client);
+        }
+
+        private Task DisplayStatsAsync(ServerStats stats)
+        {
+            _logger.LogAsync(stats.Uptime.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task LavaTrackFinishedAsync(LavaPlayer player, LavaTrack track, TrackEndReason reason)
+        {
+            if (!reason.ShouldPlayNext())
+                return;
+            
+            if (!player.Queue.TryDequeue(out var item) || !(item is LavaTrack nextTrack))
+            {
+                await player.TextChannel?.SendMessageAsync($"There are no more items left in queue.");
+                return;
+            }
+
+            await player.PlayAsync(nextTrack);
+        }
+
+        public async Task SetVolumeAsync(int vol)
+        {
+            if (_player != null && _player.IsPlaying)
+            {
+                await _player.SetVolumeAsync(vol);
+            }
+        }
+
+        public async Task ConnectAsync(SocketVoiceChannel voiceChannel, ITextChannel textChannel)
+            => await _lavaSocketClient.ConnectAsync(voiceChannel, textChannel);
+
+        public async Task LeaveAsync()
+            => await _lavaSocketClient.DisposeAsync();
+
+        public async Task<MusicEmbed> PlayAsync(string search, ulong id)
+        {
+            _player = _lavaSocketClient.GetPlayer(id);
+            var musicEmbed = new MusicEmbed();
+            var results = await _lavaRestClient.SearchYouTubeAsync(search);
+            if (results.LoadType == LoadType.NoMatches ||
+                results.LoadType == LoadType.LoadFailed)
+            {
+                musicEmbed.Title = "Nothing to play.";
+                return musicEmbed;
+            }
+
+            var track = results.Tracks.FirstOrDefault();
+            if (_player.IsPlaying)
+            {
+                _player.Queue.Enqueue(track);
+                musicEmbed.Title = $"Added To Queue: {track.Title}";
+                musicEmbed.ImageUrl = await track.FetchThumbnailAsync();
+                musicEmbed.Url = track.Uri.ToString();
+                musicEmbed.Length = track.Length;
+                musicEmbed.Author = track.Author;
+                return musicEmbed;
+            }
+            else
+            {
+                await _player.PlayAsync(track);
+                musicEmbed.Title = $"Now Playing: {track.Title}";
+                musicEmbed.ImageUrl = await track.FetchThumbnailAsync();
+                musicEmbed.Url = track.Uri.ToString();
+                musicEmbed.Length = track.Length;
+                musicEmbed.Author = track.Author;
+                return musicEmbed;
+            }
+        }
+
+        public async Task<string> StopAsync()
+        {
+            if (_player != null && _player.IsPlaying)
+            {
+                await _player.StopAsync();
+                return "Player stopped.";
+            }
+            else
+            {
+                return "Nothing is playing.";
+            }
+        }
+
+        public async Task<string> SkipAsync()
+        {
+            if (_player.Queue.Count == 0)
+            {
+                return "Nothing to skip.";
+            }
+
+            if (_player != null && _player.IsPlaying)
+            {
+                await _player.SkipAsync();
+                return $"Song Skipped, Now Playing: {_player.CurrentTrack.Title}";
+            }
+            return "Nothing is currently playing.";
+        }
+
+        public async Task<string> PauseOrResumeAsync()
+        {
+            if(_player != null && _player.IsPlaying && !_player.IsPaused)
+            {
+                await _player.PauseAsync();
+                return "Player is now paused.";
+            }
+            else
+            {
+                await _player.ResumeAsync();
+                return "Player now resumed.";
+            }
+        }
+
+        public IEnumerable<LavaTrack> GetQueue()
+        {
+            if (_player != null && _player.IsPlaying && _player.Queue.Count > 0)
+            {
+                var queue = _player.Queue.Items.Cast<LavaTrack>();
+                return queue;
+            }
+            return new List<LavaTrack>();
+        }
+
+        private async Task LavaLogAsync(LogMessage logMessage)
+        {
+            var log = LogConverter.ConvertLog(logMessage);
+            await _logger.LogMessageAsync(log);
+        }
+    }
+}
