53a6220cee2f9130a3d2a42ea0a3e699301d75ae
Joel Parkinson <joelparkinson@live.co.uk>
2019-03-31 18:44:11 +0100

    Random Test File

---
+++ MusicBot/Storage/Test.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
+using Discord.WebSocket;
+
+namespace Heaven_s_Bot.Core
+{
+    public static class DataStorage
+    {
+        public static void SaveUserAccounts(IEnumerable<UserAccount> accounts, string filePath)
+        {
+            string json = JsonConvert.SerializeObject(accounts, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static IEnumerable<UserAccount> LoaderUserAccounts(string filePath)
+        {
+            if (!File.Exists(filePath)) return null;
+            string json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<UserAccount>>(json);
+        }
+
+        public static bool SaveExists(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+    }
+}
+
+public class UserAccount
+{
+    public ulong ID { get; set; }
+    public int Checks { get; set; }
+}
+
+namespace Heaven_s_Bot.Core.UserAccounts
+{
+    public static class UserAccounts
+    {
+        public static List<UserAccount> accounts;
+
+        private static string accountsFile = "Resources/accounts.json";
+
+        static UserAccounts()
+        {
+            if (DataStorage.SaveExists(accountsFile))
+            {
+                accounts = DataStorage.LoaderUserAccounts(accountsFile).ToList();
+            }
+            else
+            {
+                accounts = new List<UserAccount>();
+                SaveAccounts();
+            }
+        }
+
+        public static void SaveAccounts()
+        {
+            DataStorage.SaveUserAccounts(accounts, accountsFile);
+        }
+
+        public static UserAccount GetAccount(SocketUser user)
+        {
+            return GetOrCreateAccount(user.Id);
+        }
+
+        public static UserAccount GetOrCreateAccount(ulong id)
+        {
+            var result = from a in accounts
+                         where a.ID == id
+                         select a;
+
+            var account = result.FirstOrDefault();
+            if (account == null) account = CreateUserAccount(id);
+            return account;
+        }
+
+        public static UserAccount CreateUserAccount(ulong id)
+        {
+            var newAccount = new UserAccount()
+            {
+                ID = id,
+                Checks = 0
+            };
+
+            accounts.Add(newAccount);
+            SaveAccounts();
+            return newAccount;
+        }
+    }
+}
