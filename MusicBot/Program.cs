7a9fd5d0a64313c9445a577a3c50ec1bddde73d5
Joel Parkinson <joelparkinson@live.co.uk>
2019-03-31 18:42:15 +0100

    Start New Music Bot

---
+++ MusicBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using MusicBot.Discord;
+using System;
+using System.Threading.Tasks;
+
+namespace MusicBot
+{
+    public class Program
+    {
+        public static async Task Main(string[] args = null)
+            => await new MusicBotClient().InitializeClientAsync();
+    }
+}
